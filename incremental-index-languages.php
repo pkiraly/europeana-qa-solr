8f65fc896763e20a3d5b255f8085b4e8d9d7e9a8
Peter Kiraly <peter.kiraly@gwdg.de>
2018-05-09 16:15:12 +0200

    Adding direct commit.

--- incremental-index-languages.php
+++ incremental-index-languages.php
@@ -24,7 +24,7 @@ while (($line = fgets($in)) != false) {
 
     if ($ln % 1000 == 0) {
       $totalTime = microtime(TRUE) - $start;
-      printf("%s/%d %s (took: %f.2/%f.2 - %f.2%%)\n", $fileName, $ln, date('H:i:s'), $totalTime, $indexTime, ($indexTime/$totalTime));
+      printf("%s/%d %s (took: %.2f/%.2f - %.2f%%)\n", $fileName, $ln, date('H:i:s'), $totalTime, $indexTime, ($indexTime/$totalTime));
       $start = microtime(TRUE);
       $indexTime = 0.0;
     }
