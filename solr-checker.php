33546cd97225fe2cff019c9316592318cd109b1f
Király Péter <kirunews@gmail.com>
2018-09-09 08:35:05 +0200

    Automatically restart Solr if it is down.

---
+++ solr-checker.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once('solr-ping.php');
+
+define('PORT', 8984);
+define('COLLECTION', 'qa-2018-08');
+define('SOLR_PATH', '/home/pkiraly/solr-7.2.1');
+
+$endTime = time() + 60;
+while (time() < $endTime) {
+  if (!isSolrAvailable(PORT, COLLECTION)) {
+    restartSolr();
+  }
+  sleep(1);
+}
+
+function restartSolr() {
+  echo date("Y-m-d H:i:s"), " restarting Solr (solr-checker)\n";
+  exec(sprintf('%s/bin/solr start -p %d -m 1g', SOLR_PATH, PORT));
+  sleep(10);
+}
