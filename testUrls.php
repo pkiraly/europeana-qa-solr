90159ee66ba6b31a66c63ab9ccbbc1967c2e2254
Peter Kiraly <peter.kiraly@gwdg.de>
2018-09-16 12:37:55 +0200

    Fix language test query.

--- testUrls.php
+++ testUrls.php
@@ -3,7 +3,7 @@
 $urls = [
   'completeness' => 'http://localhost:8984/solr/qa-2018-08/select?q=*:*%20NOT%20collection_i:[*%20TO%20*]',
   'multilinguality' => 'http://localhost:8984/solr/qa-2018-08/select?q=*:*%20NOT%20provider_dc_title_taggedliterals_f:[*%20TO%20*]',
-  'languages' => 'http://localhost:8984/solr/qa-2018-08/select?q=*:*%20NOT%20distinctlanguages_in_providerproxy_f:[*%20TO%20*]'
+  'languages' => 'http://localhost:8984/solr/qa-2018-08/select?q=*:*%20NOT%20languages_ss:[*%20TO%20*]'
 ];
 
 foreach ($urls as $feature => $url) {
