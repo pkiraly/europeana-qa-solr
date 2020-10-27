9c96dbaaef6a465e0151b03a4ee23562aba542c5
Péter Király <kirunews@gmail.com>
2020-10-27 09:06:13 +0100

    Fix the language count

--- check-solr-counts.sh
+++ check-solr-counts.sh
@@ -17,5 +17,5 @@ printf "crd_PROVIDER_Proxy_rdf_about_i:[0 TO *]:\n\t%d\n" $count_completeness
 count_multilinguality=$(curl -s "http://localhost:8984/solr/$VERSION/select?q=NumberOfLanguagesPerPropertyInProviderProxy_f%3A%5B0%20TO%20*%5D" | jq .response.numFound)
 printf "NumberOfLanguagesPerPropertyInProviderProxy_f:[0 TO *]:\n\t%d\n" $count_completeness
 
-count_languages=$(curl -s "http://localhost:8984/solr/v2019-10/select?q=languages_ss%3A*&rows=0" | jq .response.numFound)
+count_languages=$(curl -s "http://localhost:8984/solr/$VERSION/select?q=languages_ss%3A*&rows=0" | jq .response.numFound)
 printf "languages_ss:*:\n\t%d\n" $count_languages
